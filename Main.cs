aa0875daf3cf16af723cdff22fbdfef1c7037028
cxfksword <cxfksword@gmail.com>
2015-09-15 23:08:01 +0800

    First commit

---
+++ Main.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Wox.Plugin.Timestamp
+{
+    public class Main : IPlugin
+    {
+        private static readonly Regex regTimestamp = new Regex(@"^1\d{9}$", RegexOptions.IgnoreCase);
+
+        public List<Result> Query(Query query)
+        {
+            var list = new List<Result>();
+
+            var q = query.Search.Trim();
+
+            // convert timestamp to frendly date
+            if (q.Length == 10)
+            {
+                var match = regTimestamp.Match(q);
+                if (!match.Success)
+                {
+                    return list;
+                }
+
+                var timestamp = int.Parse(q);
+                var date = UnixTimeStampToDateTime(timestamp).ToString();
+                list.Add(new Result()
+                {
+                    IcoPath = "Images\\app.png",
+                    Title = date,
+                    SubTitle = "Copy date to clipboard",
+                    Action = (c) =>
+                    {
+                        Clipboard.SetText(date);
+                        return true;
+                    }
+                });
+
+                return list;
+            }
+
+            // convert datetime to timestamp
+            if (q.Length > 0)
+            {
+                DateTime date;
+                if (DateTime.TryParse(q, out date))
+                {
+                    var timestamp = DateTimeToUnixTimeStamp(date).ToString();
+                    list.Add(new Result()
+                    {
+                        IcoPath = "Images\\app.png",
+                        Title = timestamp,
+                        SubTitle = "Copy unix timestamp to clipboard",
+                        Action = (c) =>
+                        {
+                            Clipboard.SetText(timestamp);
+                            return true;
+                        }
+                    });
+
+                    return list;
+                }
+            }
+
+            // show current timestamp
+            if (q == "")
+            {
+                var timestamp = DateTimeToUnixTimeStamp(DateTime.Now).ToString();
+                list.Add(new Result()
+                {
+                    IcoPath = "Images\\app.png",
+                    Title = timestamp,
+                    SubTitle = "Copy unix timestamp to clipboard",
+                    Action = (c) =>
+                    {
+                        Clipboard.SetText(timestamp);
+                        return true;
+                    }
+                });
+            }
+
+            return list;
+        }
+
+        public void Init(PluginInitContext context)
+        {
+        }
+
+        public static DateTime UnixTimeStampToDateTime(int unixTimeStamp)
+        {
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds((double)unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+        public int DateTimeToUnixTimeStamp(DateTime date)
+        {
+            var timeSpan = (date.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (int)timeSpan.TotalSeconds;
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Wox.Plugin.Timestamp
-An Wox plugin that easy to converting between timestamps and formatted datetime strings. 
+﻿#Description
+An [Wox](https://github.com/qianlifeng/Wox) plugin that easy to converting between timestamps and formatted datetime strings. 
+
+![screenshot: `timestamp `](http://ww1.sinaimg.cn/large/7ce4a9f6gw1ew3j6n54ibj20m60b4dh5.jpg)
+
+#How to install
+
+```
+wpm install Wox.Plugin.Timestamp
+```
+
+#Usage
+1. Type `tt` in Wox to activate plugin
